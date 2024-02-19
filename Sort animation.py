12371163302e42e12ce36c25162076d3010b171f
Frees-Ling <freesling496@gmail.com>
2024-02-19 10:15:57 +0800

    Delete Sort animation.py

--- Sort animation.py
+++
@@ -1,8 +0,0 @@
-from manimlib import *
-
-class SortScene(Scene):
-    def construct(self) -> None:
-        rect = Rectangle(height=FRAME_HEIDGT,width=1)
-        rect.set_fill(opacity=1)
-        rect.set_stroke(opacity=0)
-        self.add(rect)
